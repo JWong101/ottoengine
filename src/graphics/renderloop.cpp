cfebc407fb6d2ac98100345ba2ee648c8d5c82e5
imcdo <ian_mcdonald@rocketmail.com>
2018-11-15 19:10:48 -0600

    validation layers

--- src/graphics/renderloop.cpp
+++ src/graphics/renderloop.cpp
@@ -1,5 +1,117 @@
 #include "renderloop.h"
 #include <optional>
+#include <vector>
+#include <unordered_set>
+
+
+
+
+// Debug code, Validation layers, checking what there are
+bool RenderLoop::checkValidationLayerSupport() {
+    
+    // get the avalable inctance layers
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+
+    // check to make sure all validation layers are in availableLayers i.e. make sure we have all the validation layers we claim to be using
+    // we do this by storing all acutall layer properties in a set and check if ours exist in them
+
+    std::unordered_set<std::string> availableLayerNames;
+    
+    for (const auto &layerProperties : availableLayers)) {
+        availableLayerNames.emplace(layerProperties.layerName);
+    }
+    
+    for (const std::string layerName : validationLayers) {
+        // if the layerName is inside we can maintain truth, otherwise we return false, otherwise keep checking
+        if (!availableLayerNames.count()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// gets a vector of all the GLFW extentions
+std::vector<std::string> RenderLoop::getRequiredExtensions() {
+    uint32_t glfwExtentionCount = 0;
+    const char **glfwExtentionsBasic = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    // TODO:  make sure this works, prob wont automaticly convert char * to strings
+    const std::vector<std::string> glfwExtentions {glfwExtentionsBasic, glfwExtentionsBasic + glfwExtensionCount};
+
+    if (enableValidationLayers) extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    return glfwExtentions;
+}
+
+// TODO: im pretty sure there needs to be more here
+void RenderLoop::createInstance() {
+
+    // check for validation layer support and if validation layers is enabled   
+    if (enableValidationLayers && !checkValidationLayerSupport()) {
+        throw std::runtime_error("validation layers requested, but not available!");
+    }
+
+    // search for supported extentions
+
+    // get the number of extentions, make a vector of that length, then get and save them in that vector
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+    VkApplicationInfo appInfo;
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Victoria 3"; //TODO: simplify later
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "OttoEngine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    //extensions to interact with host OS
+    uint32_t extensionCount;
+    const char **extensionNames;
+    extensionNames = glfwGetRequiredInstanceExtensions(&extensionCount);
+    createInfo.enabledExtensionCount = extensionCount;
+    createInfo.ppEnabledExtensionNames = extensionNames;
+
+    createInfo.enabledLayerCount = 0;
+
+    //TODO: disparity in code above and code bellow, determine what we want to use
+
+    auto extensions = getRequiredExtensions();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
+
+    // create VKInstance
+    if(vkCreateInstance(&createInfo, nullptr, &vkInst) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create vk instance\n");
+    }
+
+    // print out the avalable extentions
+    std::cout << "available extensions:" << std::endl;
+
+    for (const auto& extension : extensions) {
+        std::cout << "\t" << extension.extensionName << std::endl;
+    }
+}
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL RenderLoop::debugCallback (
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData) {
+    
+    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+    
+    
+    return VK_FALSE;
+}
 
 void RenderLoop::createWindow() {
     glfwInit(); 
@@ -18,6 +130,7 @@ uint32_t rateDevice(const VkPhysicalDevice &device) {
     score += device.limits.maxImageDimension2D;
     return score;
 }
+
 void Renderloop::setupPhysicalDevice() {
     uint32_t numDevices = 0;
     vkEnumeratePhysicalDevices(vkInst, &numDevices, nullptr);
@@ -36,46 +149,37 @@ void Renderloop::setupPhysicalDevice() {
             physDevice = dev;
         }
     }
-     
 }
 
 void Renderloop::checkSupportedExtensions() {
     uint32_t numSupportedExtensions = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, nullptr);
 
-    //check this later
+    // TODO: check this later
     VkExtensionProperties supportedExtensions[numExtensions];
     vkEnumerateInstance(nullptr, &numSupportedExtensions, supportedExtensions);
 }
 
 void RenderLoop::initVulkan() {
-    createInstance();
-    VkApplicationInfo appInfo;
-    info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    info.pApplicationName = "Victoria 3"; //TODO: simplify later
-    info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    info.pEngineName = "OttoEngine";
-    info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    info.apiVersion = VK_MAKE_VERSION(1, 0, 0);
-
-    VkInstanceCreateInfo createInfo;
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
-
-    //extensions to interact with host OS
-    unsigned int extensionCount;
-    const char **extensionNames;
-    extensionNames = glfwGetRequiredInstanceExtensions(&extensionCount);
-    createInfo.enabledExtensionCount = extensionCount;
-    createInfo.ppEnabledExtensionNames = extensionNames;
-
-    createInfo.enabledLayerCount = 0;
-
-    if(vkCreateInstance(&createInfo, nullptr, &vkInst) != VK_SUCCESS) {
-        throw std::runtime_error("Failed to create vk instance\n");
-    }
-
     
+    createInstance();
+    
+
+    // select a supported VkPhysicalDevice
+    setupPhysicalDevice()
+
+    // create a VkDevice and a VkQueue
+
+
+    // create a window and a swap chain(? idk whats handled by GLFW)
+
+    // create a render pass that specifies the render targets and usage
+
+    // Create framebuffers for the render pass
+
+    // Set up the graphics pipeline (VkPipeline)
+
+    // more stuff   
 }
 
 void RenderLoop::mainloop() {
--- src/graphics/renderloop.h
+++ src/graphics/renderloop.h
@@ -1,18 +1,48 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <string>
 class RenderLoop {
 
 private:
+    // Default window size
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
+
+    // Validation Layer stuff used to debug
+    const std::vector<const std::string> validationLayers = {
+        "VK_LAYER_LUNARG_standard_validation"
+    };
+
+    #ifdef NDEBUG
+        const bool enableValidationLayers = false;
+    #else
+        const bool enableValidationLayers = true;
+    #endif
+
     GLFWwindow *window;
     VkInstance *vkInst;
     const uint32_t WIDTH = 600;
     const uint32_t HEIGHT = 400;
+    bool checkValidationLayerSupport();
+    
+    std::vector<std::string> getRequiredExtensions(); 
+
+    void createInstance();
     void createWindow();
     void initVulkan();
     void init();
     void mainLoop();
     void cleanup();
     void checkSupportedExtensions();
+   
+   
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData);
 public:
     void run();
 }
+ 
